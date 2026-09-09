bc8c1e6f59164acbc5cc260f8f6bc667e7afeb77
Joaquim <jluis@ualg.pt>
2026-09-09 23:41:46 +0100

    changes in unistd.h

--- unistd.h
+++ unistd.h
@@ -1,24 +1,28 @@
-#ifndef __unistd_h__
-#define __unistd_h__ 1
+#ifndef GMTSAR_COMPAT_UNISTD_H
+#define GMTSAR_COMPAT_UNISTD_H
 
-#include <direct.h>
-#include <process.h>
-#include <io.h>
+#ifdef _WIN32
+
+#	include <direct.h>
+#	include <process.h>
+#	include <io.h>
 
 #ifndef PATH_MAX
 #	define PATH_MAX 260
 #endif
 
-#ifdef WIN32
 #	define R_OK 04
 #	define W_OK 02
 #	define X_OK 01
 #	define F_OK 00
-#endif
 
-#ifdef _WIN32
 	/* POSIX mkdir accepts a mode; the MSVC CRT's _mkdir does not. */
 #	define mkdir(path, mode) _mkdir(path)
+
+#else
+	/* This compatibility header can precede the system include directories in
+	 * the Autotools build.  Forward Unix builds to their native header. */
+#	include_next <unistd.h>
 #endif
 
-#endif
+#endif /* GMTSAR_COMPAT_UNISTD_H */
